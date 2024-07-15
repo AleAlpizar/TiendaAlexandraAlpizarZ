d477668a59d11488a42c4accddafd680d3bf7c68
AleAlpizar <133933772+AleAlpizar@users.noreply.github.com>
2024-07-14 23:14:31 -0600

    Semana 8 (mas cosas que no se agregaron)

--- TiendaAlexandraA/src/test/java/com/tienda/domain/Producto.java
+++ TiendaAlexandraA/src/test/java/com/tienda/domain/Producto.java
@@ -3,11 +3,50 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tienda.domain;
+import jakarta.persistence.*;​
+import java.io.Serializable;
+import lombok.Data;​
 
 /**
  *
  * @author 11alp
  */
-public class Producto {
+@Data​
+@Entity​
+@Table(name="producto")​
+public class Producto implements Serializable {​
+    private static final long serialVersionUID = 1L;​
+
+    @Id​
+    @GeneratedValue(strategy = GenerationType.IDENTITY)​
+    @Column(name="id_producto")​
+    private Long idProducto;    ​
+    private String descripcion;​
+    private String detalle;​
+    private double precio;​
+    private int existencias;​
+    private String rutaImagen;​
+    private boolean activo;​
+
+    @OneToMany
+    @JoinColumn(name="id_categoria")
+    List<Producto> productos;
     
+    @ManyToOne​
+    @JoinColumn(name="id_categoria")​
+    Categoria categoria;​
+
+    public Producto() {        ​
+    }​
+
+
+    public Producto(String descripcion, String detalle, double precio, int existencias, String imagen, boolean activo) {​
+        this.descripcion = descripcion;​
+        this.detalle = detalle;​
+        this.precio = precio;​
+        this.existencias = existencias;​
+        this.rutaImagen = imagen;​
+        this.activo = activo;​
+    }​
 }
+
